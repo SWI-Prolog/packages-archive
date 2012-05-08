4807fb2c1317f263958b3f519c3131d5ac7f43c3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-05-08 22:29:08 +0200

    Update documentation

--- archive.pl
+++ archive.pl
@@ -52,61 +52,69 @@ The following example lists the entries in an archive:
 	   ).
   ==
 
+@see http://code.google.com/p/libarchive/
 */
 
 :- use_foreign_library(foreign(archive4pl)).
 
-%%	archive_open(+Data, -Handle, +Options) is det.
+%%	archive_open(+Stream, -Archive, +Options) is det.
 %
-%	Create an access handle to an archive. The following options are
-%	defined:
+%	If Stream is a (binary) stream   that  contains a valid archive,
+%	unify Archive with a handle that  provides access to the content
+%	of the archive. Details are   controlled  by Options. Typically,
+%	the option close_parent(true) is used  to   close  stream if the
+%	archive is closed using archive_close/1.  For other options, the
+%	defaults are typically fine. The option format(raw) must be used
+%	to process compressed  streams  that   do  not  contain explicit
+%	entries (e.g., gzip'ed data) unambibuously.
+%
+%	  * close_parent(+Boolean)
+%	  If this option is =true= (default =false=), Stream is closed
+%	  if archive_close/1 is called on Archive.
 %
 %	  * compression(+Compression)
 %	  Support the indicated compression. This option may be used
 %	  multiple times to support multiple compression types.
 %	  If no compression options are provided, =all= is assumed.
-%	  Supported values are:
-%
-%	    - =all=
-%	    - =bzip2=
-%	    - =compress=
-%	    - =gzip=
-%	    - =lzma=
-%	    - =none=
-%	    - =xz=
+%	  Supported values are =all=, =bzip2=, =compress=, =gzip=,
+%	  =lzma=, =none= and =xz=.  The value =all= is default.
 %
 %	  * format(+Format)
 %	  Support the indicated format.  This option may be used
 %	  multiple times to support multiple formats. If no format
 %	  options are provided, =all= is assumed. Supported values are:
-%
-%	    - =all=
-%	    - =ar=
-%	    - =cpio=
-%	    - =empty=
-%	    - =iso9660=
-%	    - =mtree=
-%	    - =raw=
-%	    - =tar=
-%	    - =zip=
-%
-%	  * options ...?
-%
-%	@param ReadWrite is one of =read= or =write=
+%	  =all=, =ar=, =cpio=, =empty=, =iso9660=, =mtree=, =raw=,
+%	  =tar= and =zip=.  The value =all= is default.
 
-%%	archive_next_header(+Handle, -Name)
+%%	archive_close(+Archive) is det.
 %
-%	NextHeaderHandle is a handle to the next header in the archive.
+%	Close the archive.  If  close_parent(true)   is  specified,  the
+%	underlying stream is closed too.
+
+%%	archive_next_header(+Handle, -Name) is semidet.
+%
+%	Forward to the next entry of the  archive for which Name unifies
+%	with the pathname of the entry. Fails   silently  if the name of
+%	the  archive  is  reached  before  success.  Name  is  typically
+%	specified if a  single  entry  must   be  accessed  and  unbound
+%	otherwise. The following example opens  a   Prolog  stream  to a
+%	given archive entry. Note that  _Stream_   must  be closed using
+%	close/1 and the archive  must   be  closed using archive_close/1
+%	after the data has been used.   See also setup_call_cleanup/3.
+%
+%	  ==
+%	  open_archive_entry(ArchiveFile, Entry, Stream) :-
+%	      open(ArchiveFile, read, In, [type(binary)]),
+%	      archive_open(In, Archive, [close_parent(true)]),
+%	      archive_next_header(Archive, Entry),
+%	      archive_open_entry(Archive, Stream).
+%	  ==
+
+%%	archive_open_entry(+Archive, -Stream) is det.
+%
+%	Open the current entry as a stream.  Stream must be closed.
 
 %%	archive_header_data(+Header, +Field, -Value)
 %
 %	Extract information from the header.
 
-%%	archive_close(+Archive)
-%
-%	Close the archive.  If  close_parent(true)   is  specified,  the
-%	underlying stream is closed too.
-
-%%	archive_open_entry(+Archive, -Stream)
-%
-%	Open the current entry as a stream.  Stream must be closed.
